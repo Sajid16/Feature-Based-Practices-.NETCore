6d54136f8fcc1a19aaf7368b3ae47b1309f02f31
eIDMSapp <sourav@erainfotechbd.com>
2022-03-23 17:03:51 +0600

    LINQ console application added and is working on

---
+++ LINQ/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IList<Student> studentList = new List<Student>()
+            {
+                new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
+                new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+            };
+
+            //use of where clause
+
+            //query syntax
+            //var filterResult = (from student in studentList
+            //                    where student.Age > 14 && student.Age < 21
+            //                    select new Student
+            //                    {
+            //                        StudentID = student.StudentID,
+            //                        StudentName = student.StudentName,
+            //                        Age = student.Age
+            //                    }).ToList();
+            //foreach (var student in filterResult)
+            //{
+            //    Console.WriteLine(student.StudentName);
+            //}
+
+            ////method syntax
+            //var filterResult2 = studentList.Where(student => student.Age == 13 || student.Age == 21).ToList();
+            //foreach (var student in filterResult2)
+            //{
+            //    Console.WriteLine(student.StudentName);
+            //}
+
+            //use of where clause
+        }
+    }
+}
