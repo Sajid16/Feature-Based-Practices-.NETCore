1e871537d4a49a272d8b33c7c549a2b4a7bea4ec
Sajid16 <sajid.mahboob16@gmail.com>
2022-04-15 03:44:45 +0600

    practiced SelectMany in LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -9,37 +9,84 @@ namespace LINQ
         static void Main(string[] args)
         {
 
+            //IList<Student> studentList = new List<Student>()
+            //{
+            //    new Student() { StudentID = 1, StudentName = "John", Age = 13, ProgrammingLanguages = new List<string>{ "vvvvv", "C#", "JavaScript"} } ,
+            //    new Student() { StudentID = 2, StudentName = "Moin",  Age = 21, ProgrammingLanguages = new List<string>{ "C#", "C++", "TypeScript"}  } ,
+            //    new Student() { StudentID = 3, StudentName = "Bill",  Age = 18, ProgrammingLanguages = new List<string>{ "Mode.js", "C", "JavaScript"}  } ,
+            //    new Student() { StudentID = 4, StudentName = "Ram" , Age = 20, ProgrammingLanguages = new List<string>{ "PLSQL", "APEX", ".NetCore"} } ,
+            //    new Student() { StudentID = 5, StudentName = "Ron" , Age = 15, ProgrammingLanguages = new List<string>{ "Linq", "Angular", "TypeScript"}  }
+            //};
+
             IList<Student> studentList = new List<Student>()
             {
-                new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
-                new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
-                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
-                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
-                new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+                new Student() { StudentID = 1, StudentName = "John", Age = 13, ProgrammingLanguages = new List<Techs>{ 
+                    new Techs() { Technology = "Linq" }, 
+                    new Techs() { Technology = "C#" }, 
+                    new Techs() { Technology = "JavaScript" } } } ,
+                new Student() { StudentID = 2, StudentName = "Moin",  Age = 21, ProgrammingLanguages = new List<Techs>{
+                    new Techs() { Technology = "Linq" },
+                    new Techs() { Technology = "C#" },
+                    new Techs() { Technology = "JavaScript" } }  } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18, ProgrammingLanguages = new List<Techs>{
+                    new Techs() { Technology = "Linq" },
+                    new Techs() { Technology = "C#" },
+                    new Techs() { Technology = "JavaScript" } }  } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20, ProgrammingLanguages = new List<Techs>{
+                    new Techs() { Technology = "Linq" },
+                    new Techs() { Technology = "C#" },
+                    new Techs() { Technology = "JavaScript" } } } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 15, ProgrammingLanguages = new List<Techs>{
+                    new Techs() { Technology = "Linq" },
+                    new Techs() { Technology = "C#" },
+                    new Techs() { Technology = "JavaScript" } }  }
             };
 
+            //---------------------------------------------------------------------
             // use of select clause
 
             //var queryResult = (from student in studentList
             //                   select student).ToList();
-            var queryResult = (from student in studentList
-                               select new
-                               {
-                                   studentName = student.StudentName,
-                                   studentId = student.StudentID
-                               }).ToList();
+            //var queryResult = (from student in studentList
+            //                   select new
+            //                   {
+            //                       studentName = student.StudentName,
+            //                       studentId = student.StudentID
+            //                   }).ToList();
             //var methodResult = studentList.Select(student => student.Age).ToList();
-            var methodResult = studentList.Select(student => new 
-            {
-                studentName = student.StudentName,
-                studentAge = student.Age
-            }).ToList();
+            //var methodResult = studentList.Select(student => new 
+            //{
+            //    studentName = student.StudentName,
+            //    studentAge = student.Age
+            //}).ToList();
 
-            foreach (var student in queryResult)
-            {
-                Console.WriteLine(student.studentName + " -> "+student.studentId);
-            }
+            //foreach (var student in queryResult)
+            //{
+            //    Console.WriteLine(student.studentName + " -> "+student.studentId);
+            //}
             // use of select clause
+
+            // --------------------------------------------------------------------
+
+            // use of selectmany clause
+
+            //var queryResult = (from student in studentList
+            //                   from skill in student.ProgrammingLanguages
+            //                   select skill).Distinct().ToList();
+
+            //var methodResult = studentList.SelectMany(student => student.ProgrammingLanguages).Distinct().ToList();
+
+            var queryResult = (from student in studentList
+                               from skill in student.ProgrammingLanguages
+                               select skill).ToList();
+
+            var methodResult = studentList.SelectMany(student => student.ProgrammingLanguages).ToList();
+
+            foreach (var studentInfo in queryResult)
+            {
+                Console.WriteLine(studentInfo.Technology);
+            }
+            // use of selectmany clause
 
             // --------------------------------------------------------------------
 
--- LINQ/Student.cs
+++ LINQ/Student.cs
@@ -11,6 +11,13 @@ namespace LINQ
         public int StudentID { get; set; }
         public string StudentName { get; set; }
         public int Age { get; set; }
+        //public List<string> ProgrammingLanguages { get; set; }
+        public List<Techs> ProgrammingLanguages { get; set; }
 
+    }
+
+    class Techs
+    {
+        public string Technology { get; set; }
     }
 }
